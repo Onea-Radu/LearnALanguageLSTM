6665f6174d426502caf6201bed3c784b0b9ede5a
Onea Radu <radu-andrei.onea@my.fmi.unibuc.ro>
2020-02-23 01:28:30 +0200

    visual update and checkpoints

--- learn a language.py
+++ learn a language.py
@@ -2,6 +2,7 @@ import ctypes
 from sklearn import model_selection
 import numpy as np
 import pandas as pd
+import random
 hllDll = ctypes.WinDLL("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v10.0\\bin\\cudart64_100.dll")
 hllDll1 = ctypes.WinDLL("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v10.0\\bin\\cublas64_100.dll")
 hllDll2 = ctypes.WinDLL("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v10.0\\bin\\cudnn64_7.dll")
@@ -19,8 +20,11 @@ with open('anna.txt', 'r') as f:
 vocab = sorted(set(text))
 vocab_to_int = {c: i for i, c in enumerate(vocab)}
 int_to_vocab = dict(enumerate(vocab))
-encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
-encoded=keras.utils.to_categorical(encoded,74,'int32')
+encoded = [vocab_to_int[c] for c in text]
+encoded=keras.utils.to_categorical(encoded)
+print(encoded.shape)
+encoded=np.asarray(encoded)
+
 
 '''
 for preprocessing
@@ -31,37 +35,57 @@ with open('anna.txt', 'w') as f:
 
 def createDataSet(D,look):
     X,y=[],[]
-    for i in range(len(D)-look-1):
-        X.append(D[i:(i+look)])
+    for i in range(0,len(D)-look-1,1):
+        X.append(D[i:i+look])
         y.append(D[i+look])
         
-    return np.array(X),np.array(y)       
+    return np.asarray(X),np.asarray(y)       
 
+def inttovector(n):
+    a=[0. for i in range(74)]
+    a[n]=1
+    return a
 
-
-look=9
-X,y=createDataSet(encoded,look)
-
-X=X[0:len(X)//10]
-y=y[0:len(y)//10]
+look=12
+X,y=createDataSet(encoded[:len(encoded)//50],look)
+print(X.shape)
+X=X.reshape(X.shape[0],look,74)
 print(X.shape,y.shape)
 
-save=keras.callbacks.ModelCheckpoint('checkpoints/weights.{epoch:02d}-{categorical_accuracy:.2f}.hdf5',monitor='categorical_accuracy',period=5)
-hidsize=74
+es=keras.callbacks.EarlyStopping(monitor='categorical_accuracy',patience=10)
+save=keras.callbacks.ModelCheckpoint('checkpoints/weights.{epoch:02d}-{categorical_accuracy:.2f}.hdf5',monitor='categorical_accuracy',period=10)
+hidsize=2500
 model=keras.Sequential()
-model.add(keras.layers.Input((look,74,)))
-model.add(keras.layers.LSTM(hidsize,return_sequences=True,activation='sigmoid'))
+#model.add(keras.layers.Input((look,74)))
+#model.add(keras.layers.LSTM(hidsize,return_sequences=True,activation='sigmoid'))
 model.add(keras.layers.LSTM(hidsize,activation='sigmoid'))
-model.add(keras.layers.Dropout(0.4))
-model.add(keras.layers.Dense(700,'relu'))
-model.add(keras.layers.Dense(700,'relu'))
-model.add(keras.layers.Dropout(0.4))
-model.add(keras.layers.Dense(700,'relu'))
-model.add(keras.layers.Dense(700,'relu'))
+#model.add(keras.layers.Dropout(0.4))
+#model.add(keras.layers.Dense(30,'relu'))
 model.add(keras.layers.Dense(74,'softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-model.summary()
-model.fit(X,y,epochs=501,callbacks=[save],verbose=2)
+model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['categorical_accuracy'])
+
+ep=0
+
+
+def on_epoch_end(epoch, _):
+    global ep
+    ep+=1
+    if ep%5!=0:
+        return
+    print('*************************************************************************')
+    r=random.randrange(len(X)-look-1)
+    s=encoded[r:r+look].tolist()
+    for z in range(160):    
+        l=s.copy()
+        h=model.predict_classes(np.asarray(l).reshape(1,look,74))[0]
+        i=int_to_vocab[np.argmax(s.pop(0))]
+        print(i,end='')
+        s.append(inttovector(h))
+    print('*************************************************************************')
+end=keras.callbacks.LambdaCallback(on_epoch_end=on_epoch_end)
+
+
+model.fit(X,y ,epochs=5000,callbacks=[save,end,es],verbose=2,use_multiprocessing=True,batch_size=256)
 
 
 
--- model-checker.py
+++ model-checker.py
@@ -14,44 +14,45 @@ def inttovector(n):
     
 
 
-
-
-
 with open('anna.txt', 'r') as f:
     text=f.read()
     #X=text.split('\n')
 vocab = sorted(set(text))
 vocab_to_int = {c: i for i, c in enumerate(vocab)}
 int_to_vocab = dict(enumerate(vocab))
-encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
-encoded=keras.utils.to_categorical(encoded,74,'int32')
+encoded = [vocab_to_int[c] for c in text]
+encoded=keras.utils.to_categorical(encoded)
+
+encoded=np.asarray(encoded)
+
 
 def createDataSet(D,look):
     X,y=[],[]
-    for i in range(len(D)-look-1):
-        X.append(D[i:(i+look)])
+    for i in range(0,len(D)-look-1):
+        X.append(D[i:i+look])
         y.append(D[i+look])
         
-    return np.array(X),np.array(y)       
-
-look=9
-X,y=createDataSet(encoded,look)
-s=X[len(X)//302].tolist()
-l=s.copy()
-z=(np.array([l,])).astype('float32')
+    return np.asarray(X),np.asarray(y)    
 
 
 
+look=7
+X,y=createDataSet(encoded[:len(encoded)//100],look)
+s=X[len(X)-1300].tolist()
 
 
-model=keras.models.load_model('checkpoints/weights.06-0.17.hdf5')
 
-for i in range(120):
-    h=model.predict_classes(z)[0]
-    print(int_to_vocab[np.argmax(z[0][0])],end='')
-    s.pop(0)
-    s.append(inttovector(h))
+
+
+
+
+
+model=keras.models.load_model('checkpoints/weights.33-0.75.hdf5')
+i='a'
+#while i!=' ':
+for z in range(200):
     l=s.copy()
-    z=(np.array([l,])).astype('float32')
-
-    
+    h=model.predict_classes(np.asarray(l).reshape(1,look,74))[0]
+    i=int_to_vocab[np.argmax(s.pop(0))]
+    print(i,end='')
+    s.append(inttovector(h))
